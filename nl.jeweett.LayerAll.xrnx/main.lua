5ad0a4fba3d6881506052f0608ec35f550ea26b3
Cas <casmarrav@gmail.com>
2012-09-07 23:33:49 +0200

    big bug! fixed

--- nl.jeweett.LayerAll.xrnx/main.lua
+++ nl.jeweett.LayerAll.xrnx/main.lua
@@ -1,3 +1,20 @@
+--main
+function main()
+  local rs = renoise.song()
+  local ci = rs.selected_instrument
+  local rk = 48
+  if #ci.sample_mappings[1] > 0 then rk = ci.sample_mappings[1][1].base_note end
+  local x
+  for x = 1, #ci.sample_mappings[1], 1 do
+    ci:delete_sample_mapping_at(1, 1)
+  end
+  for x = 1, #ci.samples do
+    ci:insert_sample_mapping( 1, x, rk )
+  end
+end
+
+
+--menu
 for _,menu in pairs {"Instrument Box", "Sample List"} do
   local function multi_instr_selected()
     return (#renoise.song().selected_instrument.samples > 1 and not renoise.song().selected_instrument.sample_mappings[1].read_only)
@@ -12,16 +29,8 @@ for _,menu in pairs {"Instrument Box", "Sample List"} do
   }
 end
 
-function main()
-  local rs = renoise.song()
-  local ci = rs.selected_instrument
-  local rk = 48
-  if #ci.sample_mappings[1] > 0 then rk = ci.sample_mappings[1][1].base_note end
-  local x
-  for x = 1, #ci.sample_mappings[1], 1 do
-    ci:delete_sample_mapping_at(1, 1)
-  end
-  for x = 1, #ci.samples do
-    ci:insert_sample_mapping( 1, x, rk )
-  end
-end
+--keys
+renoise.tool():add_keybinding {
+  name = "Global:Tools:Duplicate Sample",
+  invoke = main
+}
