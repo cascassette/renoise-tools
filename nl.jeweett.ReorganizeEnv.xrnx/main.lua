4784b138651cebe95bd2923c88ae5dde2efe6056
Cas <casmarrav@gmail.com>
2012-09-18 23:13:50 +0200

    Additions - show all used, show only #Sends

--- nl.jeweett.ReorganizeEnv.xrnx/main.lua
+++ nl.jeweett.ReorganizeEnv.xrnx/main.lua
@@ -7,7 +7,7 @@
 -- Main functions
 --------------------------------------------------------------------------------
 
-local function minimize_deactivated()
+local function minimize_deactivated(excl)
   local rs = renoise.song()
   local st = rs.selected_track
   local cd
@@ -15,9 +15,23 @@ local function minimize_deactivated()
     --print ("dev# " .. i .. " :  " .. cd.display_name)
     if not cd.is_active then
       cd.is_maximized = false
+    elseif excl then
+      cd.is_maximized = true
     end
   end
 end
+
+--[[local function maximize_activated()
+  local rs = renoise.song()
+  local st = rs.selected_track
+  local cd
+  for i, cd in ipairs(st.devices) do
+    --print ("dev# " .. i .. " :  " .. cd.display_name)
+    if cd.is_active then
+      cd.is_maximized = true
+    end
+  end
+end]]
 
 local function minimize_external()
   local rs = renoise.song()
@@ -44,6 +58,20 @@ local function deactivate_unused_meta()
   end
 end
 
+local function show_only_sends()
+  local rs = renoise.song()
+  local st = rs.selected_track
+  local dc = #st.devices
+  local cd
+  for i, cd in ipairs(st.devices) do
+    if not ( cd.name == "#Send" or cd.name == "#Multiband Send" ) then
+      cd.is_maximized = false
+    else
+      cd.is_maximized = true
+    end
+  end
+end
+
 
 --------------------------------------------------------------------------------
 -- Menu entries
@@ -51,7 +79,7 @@ end
 
 renoise.tool():add_menu_entry {
   name = "Main Menu:Tools:CasTools:Reorganize DSPs:Minimize all deactivated (CT)",
-  invoke = minimize_deactivated
+  invoke = function() minimize_deactivated(false) end
 }
 
 renoise.tool():add_menu_entry {
@@ -61,14 +89,23 @@ renoise.tool():add_menu_entry {
 
 renoise.tool():add_menu_entry {
   name = "Main Menu:Tools:CasTools:Reorganize DSPs:Minimize all external and deactivated (CT)",
-  invoke = function() minimize_deactivated() minimize_external() end
+  invoke = function() minimize_deactivated(false) minimize_external() end
 }
 
 renoise.tool():add_menu_entry {
   name = "Main Menu:Tools:CasTools:Reorganize DSPs:Drop all unused lfos/envelopes (CT)",
-  invoke = function() deactivate_unused_meta() minimize_deactivated() end
+  invoke = function() deactivate_unused_meta() minimize_deactivated(false) end
 }
 
+renoise.tool():add_menu_entry {
+  name = "Main Menu:Tools:CasTools:Reorganize DSPs:Show only sends (CT)",
+  invoke = show_only_sends
+}
+
+renoise.tool():add_menu_entry {
+  name = "Main Menu:Tools:CasTools:Reorganize DSPs:Show active (CT)",
+  invoke = function() minimize_deactivated(true) end
+}
 
 --------------------------------------------------------------------------------
 -- Key Binding
@@ -76,7 +113,7 @@ renoise.tool():add_menu_entry {
 
 renoise.tool():add_keybinding {
   name = "Global:Track DSPs:Minimize all deactivated (CT)",
-  invoke = minimize_deactivated
+  invoke = function() minimize_deactivated(false) end
 }
 
 renoise.tool():add_keybinding {
@@ -86,12 +123,22 @@ renoise.tool():add_keybinding {
 
 renoise.tool():add_keybinding {
   name = "Global:Track DSPs:Minimize all external and deactivated (CT)",
-  invoke = function() minimize_deactivated() minimize_external() end
+  invoke = function() minimize_deactivated(false) minimize_external() end
 }
 
 renoise.tool():add_keybinding {
   name = "Global:Track DSPs:Drop all unused lfos/envelopes (CT)",
-  invoke = function() deactivate_unused_meta() minimize_deactivated() end
+  invoke = function() deactivate_unused_meta() minimize_deactivated(false) end
+}
+
+renoise.tool():add_keybinding {
+  name = "Global:Track DSPs:Show only sends (CT)",
+  invoke = show_only_sends
+}
+
+renoise.tool():add_keybinding {
+  name = "Global:Track DSPs:Show active (CT)",
+  invoke = function() minimize_deactivated(true) end
 }
 
 
