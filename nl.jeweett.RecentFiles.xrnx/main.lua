b71e77e185c59fc38025a9c953a2bd99cd430ea2
Cas Marrav The Wizz Kid <casmarrav@gmail.com>
2013-05-19 23:01:20 +0200

    GUI with KB op for ReCent Files (now v2)

--- nl.jeweett.RecentFiles.xrnx/main.lua
+++ nl.jeweett.RecentFiles.xrnx/main.lua
@@ -1,9 +1,16 @@
 -------------------------------------------------------------
--- Recent Files v1 by Cas Marrav (for Renoise 2.8)         --
+-- Recent Files v2 by Cas Marrav (for Renoise 2.8)         --
 -------------------------------------------------------------
 
 
--- Main
+-- GUI stuff --
+local rs
+local dialog = nil
+local vb = nil
+local select = 1
+
+
+-- Main --
 local function ls(num)
   renoise.app():load_song(renoise.app().recently_saved_song_files[num])
 end
@@ -12,8 +19,76 @@ local function ll(num)
   renoise.app():load_song(renoise.app().recently_loaded_song_files[num])
 end
 
+-- GUI --
+local function close_dialog()
+  if ( dialog and dialog.visible ) then
+    dialog:close()
+  end
+end
+
+local function key_dialog(d, k)
+  if (k.character and k.character >= "0" and k.character <= "9") then
+    if (select==1) then
+      d:close()
+      ls(tonumber(k.character))
+    elseif (select==2) then
+      d:close()
+      ll(tonumber(k.character))
+    end
+    --loadstring("l"..select.."("..k.character..")")()
+  elseif (k.name == "right") then
+    select = 2
+    vb.views["selector_l"].style = "group"
+    vb.views["selector_s"].style = "panel"
+  elseif (k.name == "left") then
+    select = 1
+    vb.views["selector_s"].style = "group"
+    vb.views["selector_l"].style = "panel"
+  elseif ( k.name == "esc" ) then
+    close_dialog()
+  else
+    --return k
+  end
+end
+
+local function showgui()
+  vb = renoise.ViewBuilder()
+  local list_s = {}
+  local list_l = {}
+  local fn = ""
+  local dialog_content = 
+    vb:horizontal_aligner
+    {
+      vb:column { id = "list_s", vb:row{ id = "selector_s", style = "group", vb:text{ text = "Saved:" } } },
+      vb:column { id = "list_l", vb:row{ id = "selector_l", style = "panel", vb:text{ text = "Loaded:" } } },
+    }
+  for i = 1, #renoise.app().recently_saved_song_files do
+    fn = renoise.app().recently_saved_song_files[i]
+    fn = fn:sub(fn:len()-string.find(fn:reverse(),"\\")+2)
+    list_s[i] = 
+      vb:button { text = ""..i..". "..fn, released = function() ls(i) close_dialog() end }
+    vb.views['list_s']:add_child(list_s[i])
+  end
+  for i = 1, #renoise.app().recently_loaded_song_files do
+    fn = renoise.app().recently_loaded_song_files[i]
+    fn = fn:sub(fn:len()-string.find(fn:reverse(),"\\")+2)
+    list_l[i] = 
+      vb:button { text = ""..i..". "..fn, released = function() ll(i) close_dialog() end }
+    vb.views['list_l']:add_child(list_l[i])
+  end
+  dialog = renoise.app():show_custom_dialog( "Laydee", dialog_content, key_dialog )
+end
+
 
 -- Keys --
+if renoise.tool():has_keybinding("Global:Tools:Recent Files...") then
+  renoise.tool():remove_keybinding("Global:Tools:Recent Files...")
+end
+renoise.tool():add_keybinding {
+  name = "Global:Tools:Recent Files...",
+  invoke = showgui,
+}
+
 for i = 1,10 do
   if renoise.tool():has_keybinding("Global:Tools:Recently Saved File #"..i) then
     renoise.tool():remove_keybinding("Global:Tools:Recently Saved File #"..i)
