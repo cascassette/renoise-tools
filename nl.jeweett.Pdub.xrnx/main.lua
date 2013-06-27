35a4408cfad4da0fe7c972c84b48d03b62049bb3
Cas Marrav The Wizz Kid <casmarrav@gmail.com>
2013-06-27 12:16:01 +0200

    PDub includes automations

--- nl.jeweett.Pdub.xrnx/main.lua
+++ nl.jeweett.Pdub.xrnx/main.lua
@@ -1,20 +1,34 @@
 -------------------------------------------------------------
--- PDub v0.1 by Cas Marrav (for Renoise 2.8)               --
+-- PDub/PCut v0.2 by Cas Marrav (for Renoise 2.8)          --
 -------------------------------------------------------------
 
 -- Main --
-local function pdub()
+local function pdub(include_auto)
   local rs = renoise.song()
-  local curpat_index = rs.sequencer.pattern_sequence[rs.transport.edit_pos.sequence]
+  local curpat_index = rs.sequencer:pattern(rs.selected_sequence_index)
   local curpat = rs:pattern(curpat_index)
   local len = curpat.number_of_lines
+  local lina = len*256
+  local a
+  local pt
   if len <= renoise.Pattern.MAX_NUMBER_OF_LINES/2 then
     curpat.number_of_lines = curpat.number_of_lines * 2
     if not curpat.is_empty then
-      for _,t in pairs(curpat.tracks) do
-        --t:lines_in_range(len+1, 2*len) = t:lines_in_range(1, len)
-        for i,l in pairs(t.lines) do
-          t:line(i+len):copy_from(l)
+      for ti,t in pairs(curpat.tracks) do
+        for li,l in pairs(t.lines) do
+          t:line(li+len):copy_from(l)
+        end
+        if include_auto then
+          for di,d in pairs(rs:track(ti).devices) do
+            for pi,p in pairs(d.parameters) do
+              a = t:find_automation(p)
+              if a then
+                for api,ap in pairs(a.points) do
+                  a:add_point_at(ap.time+len, ap.value)
+                end
+              end
+            end
+          end
         end
       end
     end
@@ -24,18 +38,65 @@ local function pdub()
   end
 end
 
+local function pdub_ia()    -- include automation
+  local rs = renoise.song()
+  local curpat_index = rs.sequencer.pattern_sequence[rs.transport.edit_pos.sequence]
+  local curpat = rs:pattern(curpat_index)
+  local newpat_index = rs.sequencer:insert_new_pattern_at(curpat_index+1)
+  local newpat = rs:pattern(newpat_index)
+  newpat.number_of_lines = curpat_number_of_lines
+  newpat:copy_from(curpat)
+end
+
+local function pcut()
+  local rs = renoise.song()
+  local ep = rs.transport.edit_pos
+  local cpi = rs.transport.selected_pattern_index
+  local cp = rs.transport.selected_pattern
+  local lc = ep.line      -- rs.selected_line_index
+  local csi = ep.sequence
+  local inserts = table.create()
+  -- think about where to put the 'second halves'
+  for i,s in ipairs(rs.sequencer.pattern_sequence) do
+    if s == cpi then
+      inserts.add(i)
+    end
+  end
+  local first_occ = inserts[1]
+  local add = 0
+  npi = rs.sequencer:insert_new_pattern_at(csi+1)
+  for i = lc,cp.number_of_lines do
+  end
+end
+
 
 -- Menu --
 renoise.tool():add_menu_entry {
   name = "Main Menu:Tools:CasTools:PDub",
-  invoke = pdub
+  invoke = function() pdub(false) end
+}
+renoise.tool():add_menu_entry {
+  name = "Main Menu:Tools:CasTools:PDub + Auto",
+  invoke = function() pdub(true) end
+}
+renoise.tool():add_menu_entry {
+  name = "Main Menu:Tools:CasTools:PCut",
+  invoke = pcut
 }
 
 
 -- Keys --
 renoise.tool():add_keybinding {
   name = "Pattern Editor:Pattern Operations:PDub",
-  invoke = pdub
+  invoke = function() pdub(false) end
+}
+renoise.tool():add_keybinding {
+  name = "Pattern Editor:Pattern Operations:PDub + Auto",
+  invoke = function() pdub(true) end
+}
+renoise.tool():add_keybinding {
+  name = "Pattern Editor:Pattern Operations:PCut",
+  invoke = pcut
 }
 
 
