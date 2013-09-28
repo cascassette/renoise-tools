7d142ffedd7a55a9ceaf37d603c7d14d923ea83e
Johannes Kroll <jkroll@lavabit.com>
2013-09-28 11:43:59 +0200

    fix/shorten midi mapping functions for loop handling

--- nl.jeweett.MidiZoomSelect.xrnx/main.lua
+++ nl.jeweett.MidiZoomSelect.xrnx/main.lua
@@ -6,98 +6,51 @@ local rs, smp, len, step
 
 -- functions --
 local function lzoom(mm)
-  local nv = mm.int_value  -- [0..127]
-  rs = renoise.song()
-  smp = rs.selected_sample
+  local smp = renoise.song().selected_sample
   if smp.sample_buffer.has_sample_data then
-    local min = 1
-    local max = smp.sample_buffer.number_of_frames
-    len = max-min
-    step = math.floor(len/128)
-    local rzoom = smp.sample_buffer.display_range[2]
-    if nv*step+min < rzoom then
-      smp.sample_buffer.display_range = { nv*step+min, rzoom }
-    end
+    smp.sample_buffer.display_range = { (smp.sample_buffer.display_range[2]-2) * mm.int_value / 127 + 1, smp.sample_buffer.display_range[2] }
   end
 end
 local function rzoom(mm)
-  local nv = mm.int_value+1  -- [0..127]
-  rs = renoise.song()
-  smp = rs.selected_sample
+  local smp = renoise.song().selected_sample
   if smp.sample_buffer.has_sample_data then
-    local min = 1
+    local min = smp.sample_buffer.display_range[1]
     local max = smp.sample_buffer.number_of_frames
-    len = max-min
-    step = math.floor(len/128)
-    local lzoom = smp.sample_buffer.display_range[1]
-    if nv == 128 then smp.sample_buffer.display_range = { lzoom, max }
-    elseif nv*step+min > lzoom then
-      smp.sample_buffer.display_range = { lzoom, nv*step+min }
-    end
+    smp.sample_buffer.display_range = { smp.sample_buffer.display_range[1], (max-min) * mm.int_value / 127 + min + 1 }
   end
 end
 
 local function lsel(mm)
-  local nv = mm.int_value  -- [0..127]
-  rs = renoise.song()
-  smp = rs.selected_sample
-  if smp.sample_buffer.has_sample_data then
-    local min = smp.sample_buffer.display_range[1]
-    local max = smp.sample_buffer.display_range[2] --smp.sample_buffer.number_of_frames
-    len = max-min
-    step = math.floor(len/128)
-    local rsel = smp.sample_buffer.selection_end
-    if nv*step+min < rsel then
-      smp.sample_buffer.selection_start = nv*step+min
-    end
-  end
-end
-local function rsel(mm)
-  local nv = mm.int_value+1  -- [0..127]
-  rs = renoise.song()
-  smp = rs.selected_sample
+  local smp = renoise.song().selected_sample
   if smp.sample_buffer.has_sample_data then
     local min = smp.sample_buffer.display_range[1]
     local max = smp.sample_buffer.display_range[2]
-    len = max-min
-    step = math.floor(len/128)
-    local lsel = smp.sample_buffer.selection_start
-    if nv == 128 then smp.sample_buffer.selection_end = max
-    elseif nv*step+min > lsel then
-      smp.sample_buffer.selection_end = nv*step+min
-    end
+    smp.sample_buffer.selection_start = (max-min) * mm.int_value / 127 + min + 1
+  end
+end
+local function rsel(mm)
+  local smp = renoise.song().selected_sample
+  if smp.sample_buffer.has_sample_data then
+    local min = smp.sample_buffer.display_range[1]
+    local max = smp.sample_buffer.display_range[2]
+    smp.sample_buffer.selection_end = (max-min) * mm.int_value / 127 + min + 1
   end
 end
 
 local function lloop(mm)
-  local nv = mm.int_value  -- [0..127]
-  rs = renoise.song()
-  smp = rs.selected_sample
+  local smp = renoise.song().selected_sample
   if smp.sample_buffer.has_sample_data then
     local min = smp.sample_buffer.display_range[1]
-    local max = smp.sample_buffer.display_range[2]
-    len = max-min
-    step = math.floor(len/128)
-    local rloop = smp.loop_end
-    if nv*step+min < rloop then
-      smp.loop_start = nv*step+min
-    end
+    local max = math.min(smp.loop_end, smp.sample_buffer.display_range[2])
+    smp.loop_start = (max-min) * mm.int_value / 127 + min
   end
 end
 local function rloop(mm)
-  local nv = mm.int_value+1  -- [0..127]
-  rs = renoise.song()
-  smp = rs.selected_sample
+  local smp = renoise.song().selected_sample
   if smp.sample_buffer.has_sample_data then
-    local min = smp.sample_buffer.display_range[1]
+    local min = math.max(smp.loop_start, smp.sample_buffer.display_range[1])
     local max = smp.sample_buffer.display_range[2]
-    len = max-min
-    step = math.floor(len/128)
-    local lloop = smp.loop_start
-    if nv == 128 then smp.loop_end = max
-    elseif nv*step+min > lloop then
-      smp.loop_end = nv*step+min
-    end
+    smp.loop_end = (max-min-1) * mm.int_value / 127 + min
   end
 end
 
